db1bc3010346aeb476db494f15e4eef29aeab930
Olha10 <100kravchenko@gmail.com>
2021-08-08 13:51:18 +0300

    Change java version from 16 to 8, add group test_base to MainPageTests

--- src/test/java/testcases/MainPageTests.java
+++ src/test/java/testcases/MainPageTests.java
@@ -7,7 +7,7 @@ import testbase.TestBase;
 public class MainPageTests extends TestBase {
     MainPageLocators mainPageLocators = new MainPageLocators();
 
-    @Test
+    @Test(priority = 1, groups="test_base")
     public void checkIcecatSearchButton() {
         elementsHelper.clickOnVisibleAndClickableElement(mainPageLocators.IcecatSearchButton,15);
     }
